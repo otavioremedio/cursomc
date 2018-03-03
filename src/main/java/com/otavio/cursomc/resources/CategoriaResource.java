43dc2097878baeecfc2ce64ef6c3ef74b473db91
otavioremedio <otavioremedio@gmail.com>
2018-03-03 20:29:26 -0300

    Testando categoria

---
+++ src/main/java/com/otavio/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,29 @@
+package com.otavio.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.otavio.cursomc.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@GetMapping
+	public List<Categoria> listar(){
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
+	}
+
+}
